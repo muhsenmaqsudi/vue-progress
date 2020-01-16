e85b670428c90539a2358964fd651f7738214910
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-01-17 00:18:42 +0330

    added tailwindcss for css styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-blue-100">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'conditional-rendering' }">
+        Conditional Rendering
+      </router-link>
     </div>
     <router-view />
   </div>
