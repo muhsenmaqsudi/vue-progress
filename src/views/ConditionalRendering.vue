76042dfb0a5b6d53026a7e965d21fffa59ef0e99
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-01-31 10:12:32 +0330

    conditional rendering | controlling reusable elements with key

--- src/views/ConditionalRendering.vue
+++ src/views/ConditionalRendering.vue
@@ -37,6 +37,51 @@
         <div v-else>Not A/B/C</div>
       </div>
     </div>
+    <div class="flex px-4">
+      <div class="w-1/2 border-2">
+        <h2 class="text-lg font-semibold text-red-300">
+          Controlling Reusable Elements with key
+        </h2>
+        <div class="py-2">
+          <h4>Example with no key</h4>
+          <template v-if="loginType === 'username'">
+            <label>Username: </label>
+            <input placeholder="Enter your username" />
+          </template>
+          <template v-else>
+            <label>Email: </label>
+            <input placeholder="Enter your email address" />
+          </template>
+          <p>
+            witching the loginType in the code above will not erase what the
+            user has already entered. Since both templates use the same
+            elements, the input is not replaced - just its placeholder
+          </p>
+        </div>
+        <div class="py-2">
+          <h4>Example with key</h4>
+          <template v-if="loginType === 'username'">
+            <label>Username: </label>
+            <input placeholder="Enter your username" key="username-input" />
+          </template>
+          <template v-else>
+            <label>Email: </label>
+            <input placeholder="Enter your email address" key="email-input" />
+          </template>
+          <p>
+            This isn’t always desirable though, so Vue offers a way for you to
+            say, “These two elements are completely separate - don’t re-use
+            them.” Add a key attribute with unique values:
+          </p>
+        </div>
+        <button
+          class="bg-blue-300 p-1 rounded-lg hover:bg-blue-500"
+          @click="toggleLoginType"
+        >
+          Toggle login type
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,8 +92,19 @@ export default {
     return {
       awesome: true,
       ok: true,
-      type: "B"
+      type: "B",
+      loginType: "username"
     };
+  },
+  methods: {
+    toggleLoginType() {
+      console.log("y");
+      if (this.loginType === "username") {
+        this.loginType = "email";
+      } else {
+        this.loginType = "username";
+      }
+    }
   }
 };
 </script>
